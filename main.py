770d564e24a860e0a558c3cb3de6633dbb3cb6ec
Marina <megorlova@mail.ru>
2024-03-17 20:10:46 +0300

    тесты

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pytest
 
+# Тесты для структуры данных str:
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@pytest.mark.parametrize("test_string, expected_result", [("hello", "HELLO"), ("Python", "PYTHON"), ("", "")])
+def test_string_uppercase(test_string, expected_result):
+    assert test_string.upper() == expected_result
+@pytest.mark.parametrize("test_string, expected_result", [("Hello", True), ("Hello123", False), ("123", False)])
+def test_string_isalpha(test_string, expected_result):
+    try:
+        assert test_string.isalpha() == expected_result
+    except AttributeError as e:
+        pytest.fail(f"AttributeError: {e}")
+@pytest.mark.parametrize("test_string, expected_result", [("   Hello   ", "Hello"), ("   World", "World"), ("Space   ", "Space")])
+def test_string_strip(test_string, expected_result):
+    assert test_string.strip() == expected_result@pytest.mark.parametrize("test_string, to_replace, replacement, expected_result", [("Hello, World!", "Hello", "Hi", "Hi, World!")])
+def test_string_replace(test_string, to_replace, replacement, expected_result):
+    assert test_string.replace(to_replace, replacement) == expected_result
 
+# Тесты для структуры данных set:
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@pytest.mark.parametrize("set1, set2, expected_result", [({1, 2, 3}, {3, 4, 5}, {1, 2, 3, 4, 5}), ({1, 2, 3}, {3, 4, 5, 6}, {1, 2, 3, 4, 5, 6})])
+def test_set_union(set1, set2, expected_result):
+    assert set1.union(set2) == expected_result@pytest.mark.parametrize("set1, set2, expected_result", [({1, 2, 3, 4, 5}, {2, 3}, True), ({1, 2, 3, 4, 5}, {6, 7}, False)])
+def test_set_subset(set1, set2, expected_result):
+    assert set2.issubset(set1) == expected_result@pytest.mark.parametrize("set1, set2, expected_result", [({1, 2, 3, 4, 5}, {2, 3}, True), ({2, 3}, {1, 2, 3, 4, 5}, False)])
+def test_set_superset(set1, set2, expected_result):
+    assert set1.issuperset(set2) == expected_result
